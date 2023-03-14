ddc946121ada70d70461b7a62d0ef2ab961311b8
Shaunak Ghosh <98326430+Rooveloftcodes@users.noreply.github.com>
2023-03-14 22:57:37 +0530

    add nav-menu with animation

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+const toggleButton = document.getElementsByClassName('toggle-btn')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const navBar = document.getElementById('nav-bar')
+const list = document.getElementById('list')
+toggleButton.addEventListener('click', () => {
+        console.log("on")
+        navbarLinks.classList.toggle('active')
+        navBar.classList.toggle('active')
+        list.classList.toggle('active')    
+})
