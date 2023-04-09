a532571d9d392ae35701d1f99377c3d290b460f9
Shaunak Ghosh <98326430+Rooveloftcodes@users.noreply.github.com>
2023-04-09 16:15:38 +0530

    changed layout

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,10 +1,10 @@
-const toggleButton = document.getElementsByClassName('toggle-btn')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-const navBar = document.getElementById('nav-bar')
-const list = document.getElementById('list')
-toggleButton.addEventListener('click', () => {
+
+const linksToggleButton = DOCUMENT.getElementById('links-toggle-btn')[0]
+const linksPanel = document.getElementById('links-panel')
+linksToggleButton.addEventListener('click', () => {
         console.log("on")
-        navbarLinks.classList.toggle('active')
+        linksPanel.classList.toggle('active')
+        /*navbarLinks.classList.toggle('active')
         navBar.classList.toggle('active')
-        list.classList.toggle('active')
+        list.classList.toggle('active')*/
 })
