09da9074fb853679219c78ef94281b4c28eba0c4
Shaunak Ghosh <98326430+Rooveloftcodes@users.noreply.github.com>
2023-07-08 00:33:53 +0530

    add anchor text hover effect

--- scripts/sidemenu.js
+++ scripts/sidemenu.js
@@ -1,21 +1,31 @@
 var linksToggleButton = document.getElementById('links-toggle-btn');
-var linksPanel = document.getElementById('links-panel');        
+var linksPanel = document.getElementById('links-panel');
+var line1 = document.getElementsByClassName('line-1');      
 linksToggleButton.addEventListener('click', function() {
-    if (linksPanel.classList.contains('open')) 
+    if (linksPanel.classList.contains('open') && linksToggleButton.classList.contains('cross')) 
     {
         linksPanel.classList.remove('open');
+        linksToggleButton.classList.remove('open');
     } 
     else
     {
         linksPanel.classList.add('open');
+        linksToggleButton.classList.add('cross');
     }
-});            
+});       
+linksToggleButton.addEventListener('click', function() {
+    if(linksToggleButton.classList.contains('cross')) 
+    {
+        line1.style.visibility = 'hidden';        
+    }
+});
 document.addEventListener('click', function(event) 
 {
     var target = event.target;
     if (!linksPanel.contains(target) && !linksToggleButton.contains(target)) 
     {
         linksPanel.classList.remove('open');
+        linksToggleButton.classList.remove('cross');
     }
 });
 //var closeButton = document.getElementById('closeButton');
@@ -23,4 +33,4 @@ var closeButton = document.getElementsByClassName('open')
 closeButton.addEventListener('click', function() 
 {
     linksPanel.classList.remove('open');
-});  
+});
