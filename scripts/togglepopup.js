7fae49ecd31a893f418e1bb2b1f07d5835176331
Shaunak Ghosh <98326430+Rooveloftcodes@users.noreply.github.com>
2024-01-08 13:28:56 +0530

    add variable popup-window content, title

--- scripts/togglepopup.js
+++ scripts/togglepopup.js
@@ -1,37 +1,14 @@
+var popup = document.getElementById('popupContainer');
+var popupTitle = document.getElementById("popup-title");
+var popupContent = document.getElementById("popup-content");
 var closeButton = document.getElementById('close');
-var popupTitle = document.getElementById('popup-title');
-function showPopup(title) {
-    var popup = document.getElementById('popupContainer');
-    popup.style.display = 'block';
-    popupTitle.innerText = title; //paste title of clicked element
-    // Hide popup when the user clicks outside the container
-    closeButton.onclick = function(event) {
-        popup.style.display = 'none';
-    }
-    window.onclick = function(event) {
-        if (event.target == popup){
-            popup.style.display = 'none';                        
-        }
-    }
-    function handleKeyDown(event) {
-        if (event.key === 'Escape') {
-            if (popup.style.display == 'block')
-            {
-                popup.style.display = 'none';
-                popup.style.transition = 'cubic-bezier(0.03, 0.6, 0.3, 1.09)';
-            }
-        }
-    }
-    // Attach the event listener to the window object
-    window.addEventListener('keydown', handleKeyDown);
+var scrollBar = document
 
-    if(title == 'Low Poly Man')
-    {console.log(title);}
-}
-/*function changeContent(contentID) {
-    var popup = document.getElementById('popupContainer');
-    popup.style.display = 'block'; //paste title of clicked element
-    // Hide popup when the user clicks outside the container
+function showPopup(title) {
+    
+    popup.style.display = 'block';
+    popupTitle.innerText = title;
+
     closeButton.onclick = function(event) {
         popup.style.display = 'none';
     }
@@ -49,13 +26,17 @@ function showPopup(title) {
             }
         }
     }
-    
     var content;
-    if (contentID === 'Low Poly Man') {
-        content = '<style> .header-text{ color: red; align-text: center; font-size: 100px}</style> <span class="header-text"> Dummy text</span>';
+    if(title === 'Low Poly Man')
+    {
+        popupContent.style.textAlign = 'center';
+        content = '<img style="height: 100%" class="image-content" src="assets/work/illustrations/2023/low_poly_man.jpg">';
     }
-    
-    popupContainer.innerHTML = content;
+    if(title === 'Dwarfs Journey')
+    {
+        popupContent.style.textAlign = 'left';
+        content = '<header style="font-size: 35px"> A pixelart 2D platformer game</header>';
+    }
+    popupContent.innerHTML = content;
     window.addEventListener('keydown', handleKeyDown);
-    popup.style.display = 'block';
-}*/
+}
